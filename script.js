dddeaa770c15d81ae3bf86863f0ffe63008a1ef0
mkhaffi16 <80031279+mkhaffi16@users.noreply.github.com>
2021-03-04 16:14:04 +0700

    penambahkan fitur agar client dengan mudah dapat melakukan pengecekan secara berulang tanpa repot

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-alert('program memasukan angka positif dan negatif')
+let status = true;
+while ( status = true ){  
+  alert('program memasukan angka positif dan negatif')
   let angka = parseInt(prompt('masukan angka'))
 
   if (angka >= 0) {
     alert('angka ini positif');
-  } else if( angka == 0){
+  } else if (angka == 0) {
     alert('angka ini adalah 0');
   } else if (angka < 0) {
     alert('angka ini adalah negatif');
   } else {
     alert('ini bukan angka');
   }
+
+  tanya = confirm('apakah anda mau memasukan ulang?')
+  
+}
